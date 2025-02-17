5ff13adba4f389934b19ceffee1c066ddfd0b726
xAlatariel <Orlandielia82@gmail.com>
2025-02-17 01:17:33 +0100

    start

---
+++ gestione_viaggi/src/main/java/com/example/gestione_viaggi/GestioneViaggiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestione_viaggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneViaggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneViaggiApplication.class, args);
+	}
+
+}
---
+++ gestione_viaggi/src/test/java/com/example/gestione_viaggi/GestioneViaggiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestione_viaggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneViaggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
